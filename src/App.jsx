4f651e2f2da79244b26d00ddb8092a104b084add
Begüm Ircı <begumirci@Begum-MacBook-Air.local>
2024-03-19 11:53:52 +0300

    overflow değişti

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Results from './components/Results'
-import FormContent from './components/FormContent'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Results from './components/Results';
+import FormContent from './components/FormContent';
 import { getAllRecords, oneRecord } from './helper';
 import { Link } from 'react-router-dom';
 
-
-
-
-
 function App() {
   const [data, setData] = useState([]);
-
 
   useEffect(() => {
     setData(getAllRecords());
   }, []);
 
-
   return (
-    <>
-      <div className='container'> 
+    <div className='all'>
+      <div className='container'>
         <div className='result'>
           <Results data={data} setData={setData} />
         </div>
       </div>
-
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/FormContent.jsx
+++ src/components/FormContent.jsx
@@ -1,81 +1,111 @@
-import { useState, useRef } from "react";
-import { commitRecord, getAllRecords, InsertRecords, oneRecord, UpdateRecords } from "../helper";
+import { useState, useRef } from 'react';
+import {
+  commitRecord,
+  getAllRecords,
+  InsertRecords,
+  oneRecord,
+  UpdateRecords,
+} from '../helper';
 import '../App.css';
-import { useLoaderData, useNavigate } from "react-router-dom";
-import { useParams } from "react-router-dom";
-
+import { useLoaderData, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 export async function formLoader({ params }) {
-    return oneRecord(params.id);
+  return oneRecord(params.id);
 }
 
 export default function FormContent() {
-    const [submit, setSubmit] = useState(false);
-    const myForm = useRef();
-    const navigate = useNavigate();
+  const [submit, setSubmit] = useState(false);
+  const myForm = useRef();
+  const navigate = useNavigate();
 
-    const loaderData = useLoaderData();
-    
-    
+  const loaderData = useLoaderData();
 
-    function handleClick(e) {
-        e.preventDefault();
-        const formData = new FormData(myForm.current);
+  function handleClick(e) {
+    e.preventDefault();
+    const formData = new FormData(myForm.current);
 
-        const data = getAllRecords();
-        
-            if(data.find(x => x.email == Object.fromEntries(formData).email)){
-                alert('Bu mail adresiyle verimiz bulunmaktadır');
-                return;
-            }
-        if (Object.fromEntries(formData).name == '' || Object.fromEntries(formData).email == '' || Object.fromEntries(formData).gender == ''){
-            alert('Bilgileriniz Eksik');
-            return;
-        }
+    const data = getAllRecords();
 
-            
-        
-        if (loaderData) {
-            UpdateRecords(Object.fromEntries(formData));
-            alert('Veriniz Başarıyla Güncellendi!');
-        } else {
-            InsertRecords(Object.fromEntries(formData));
-        }
-
-        navigate('/');
-
+    if (data.find((x) => x.email == Object.fromEntries(formData).email)) {
+      alert('Bu mail adresiyle verimiz bulunmaktadır');
+      return;
+    }
+    if (
+      Object.fromEntries(formData).name == '' ||
+      Object.fromEntries(formData).email == '' ||
+      Object.fromEntries(formData).gender == ''
+    ) {
+      alert('Bilgileriniz Eksik');
+      return;
     }
 
+    if (loaderData) {
+      UpdateRecords(Object.fromEntries(formData));
+      alert('Veriniz Başarıyla Güncellendi!');
+    } else {
+      InsertRecords(Object.fromEntries(formData));
+    }
 
+    navigate('/');
+  }
 
-    return (
-        <div className="container">
-            <div className="myForm">
-                <form onSubmit={handleClick} autoComplete="on" ref={myForm} noValidate className={submit ? 'submit' : ''}  >
-
-                    <div className='formElement' >
-
-                        <input type="text" placeholder='İsim Soyisim Girin' required name='name' defaultValue={loaderData?.name} />
-                        <p>Lütfen isim girin</p>
-                    </div>
-                    <div className='formElement'>
-                        <input type="email" placeholder='E-posta Girin' required name='email' defaultValue={loaderData?.email} />
-                        <p>Lütfen email girin</p>
-                    </div>
-                    <div className='formElement'>
-                        <select name="gender" required defaultValue={loaderData?.gender}>
-                            <option value="">Cinsiyet</option>
-                            <option value="Kadın">Kadın</option>
-                            <option value="Erkek">Erkek</option>
-                        </select>
-                    </div>
-                    {loaderData?.id && <input type='hidden' name='id' defaultValue={loaderData?.id} />}
-                    <div className="btns">
-                        <button className='add'>{loaderData?.id ? "Güncelle" : "Ekle"}</button>
-                        <button type="button" className='add' onClick={() => navigate(-1)}>Geri</button>
-                    </div>
-                </form>
+  return (
+    <div className='all'>
+      <div className='container'>
+        <div className='myForm'>
+          <form
+            onSubmit={handleClick}
+            autoComplete='on'
+            ref={myForm}
+            noValidate
+            className={submit ? 'submit' : ''}
+          >
+            <div className='formElement'>
+              <input
+                type='text'
+                placeholder='İsim Soyisim Girin'
+                required
+                name='name'
+                defaultValue={loaderData?.name}
+              />
+              <p>Lütfen isim girin</p>
             </div>
+            <div className='formElement'>
+              <input
+                type='email'
+                placeholder='E-posta Girin'
+                required
+                name='email'
+                defaultValue={loaderData?.email}
+              />
+              <p>Lütfen email girin</p>
+            </div>
+            <div className='formElement'>
+              <select name='gender' required defaultValue={loaderData?.gender}>
+                <option value=''>Cinsiyet</option>
+                <option value='Kadın'>Kadın</option>
+                <option value='Erkek'>Erkek</option>
+              </select>
+            </div>
+            {loaderData?.id && (
+              <input type='hidden' name='id' defaultValue={loaderData?.id} />
+            )}
+            <div className='btns'>
+              <button className='add'>
+                {loaderData?.id ? 'Güncelle' : 'Ekle'}
+              </button>
+              <button
+                type='button'
+                className='add'
+                onClick={() => navigate(-1)}
+              >
+                Geri
+              </button>
+            </div>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
